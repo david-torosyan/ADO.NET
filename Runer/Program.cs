231ee542ea0aa916a50e470173fc162d2751d514
Torosyan David <davit.torosyan.0214@gmail.com>
2024-01-19 01:03:24 +0400

    Still working

--- Runer/Program.cs
+++ Runer/Program.cs
@@ -6,13 +6,13 @@ class Program
     static async Task Main(string[] args)
     {
 
-        //BaseRepository<Person> repos = new BaseRepository<Person>(new ConfigurationProvider(), "person");
-        //var entities = await repos.GetAllAsync();
+        BaseRepository<Person> repos = new BaseRepository<Person>(new ConfigurationProvider(), "person");
+        var entities = await repos.GetAllAsync();
 
-        //foreach (var item in entities)
-        //{
-        //    await Console.Out.WriteLineAsync($"{item.name} - {item.surname}");
-        //}
+        foreach (var item in entities)
+        {
+            await Console.Out.WriteLineAsync($"{item.name} - {item.surname}");
+        }
 
     }
 }
